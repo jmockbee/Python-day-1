a0deddb24f466fae08940c769134c8bddcad16f3
Jon Mockbee <mockbee1000@gmail.com>
2021-07-14 19:30:49 -0400

    First commit for day 1

---
+++ test.py
@@ -0,0 +1,8 @@
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*")
+print()
+print("The itsy bitsy spider\nclimbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
+
